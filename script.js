8aae6e59500ab7f8b54594e2203546a29bd16fbe
Asrithakurra <kurraasritha26@gmail.com>
2024-10-27 23:00:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function redButton(){
+    document.getElementById("redCircle").style.backgroundColor="#cf1124";
+    document.getElementById("red").style.backgroundColor="#cf1124";
+    document.getElementById("yellowCircle").style.backgroundColor="#4b5069";
+    document.getElementById("yellow").style.backgroundColor="#1f1d41";
+    document.getElementById("greenCircle").style.backgroundColor="#4b5069";
+    document.getElementById("green").style.backgroundColor="#1f1d41";   
+
+}
+function yellowButton(){
+ 
+    document.getElementById("yellowCircle").style.backgroundColor="#f7c948";
+    document.getElementById("yellow").style.backgroundColor="#f7c948";
+    document.getElementById("redCircle").style.backgroundColor="#4b5069";
+    document.getElementById("red").style.backgroundColor="#1f1d41";
+    document.getElementById("greenCircle").style.backgroundColor="#4b5069";
+    document.getElementById("green").style.backgroundColor="#1f1d41";   
+}
+function greenButton(){
+ 
+    document.getElementById("greenCircle").style.backgroundColor="#199473";
+    document.getElementById("green").style.backgroundColor="#199473";  
+    document.getElementById("redCircle").style.backgroundColor="#4b5069";
+    document.getElementById("red").style.backgroundColor="#1f1d41";
+    document.getElementById("yellowCircle").style.backgroundColor="#4b5069";
+    document.getElementById("yellow").style.backgroundColor="#1f1d41"; 
+}
